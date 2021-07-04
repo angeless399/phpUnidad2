1353bc4fbe3249488d0c69afade2db3083c8e7ee
angeless399 <angeless.399@gmail.com>
2021-07-04 16:37:26 -0300

    Actividad obligatoria PHP unidad 2

---
+++ PHP_Act_oblig_unidad2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style type="text/css">
+	main {
+		width: 100%;
+		max-width: 500px;
+		margin: auto;
+		padding: 15px;
+		border-radius: 15px;
+		background: orange;
+	}
+	p {text-indent: 20%;}
+	h2 {text-align: center;}
+	</style>
+</head>
+<body>
+<main>
+	<h2>Actividad Práctica Obligatoria Unidad 2 PHP</h2>
+	<?php
+	echo '<p><b>NOMBRE: </b> María de los Angeles <br></p>';
+	echo '<p><b>APELLIDO: </b> López<br></p>';
+	echo '<p><b>EDAD: </b> 50 <br></p>';
+	echo '<p><b>HOBBIE: </b> Viajar <br></p>';
+	echo '<p><b>EDITOR: </b> Sublime text <br></p>';
+	echo '<p><b>S. OPERATIVO: </b> windows <br></p>';
+	?>
+</main>
+</body>
+</html>
+
